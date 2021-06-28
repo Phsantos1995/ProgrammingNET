6278589df83191db3ade33d5cab37610695e32e4
8502153 <edsonfr@sme.prefeitura.sp.gov.br>
2021-06-28 19:44:45 -0300

    treino

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using   System;
+
+
+namespace ConsoleApp2
+{ 
+    class Program
+    {
+
+
+        private string name;
+        private int idade;
+
+        public Program()
+        {
+            Console.WriteLine("Foi criando um objeto pessoa");
+
+        }
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+        public class Pessoa : Program
+        {
+            public string matricula;
+
+            public Pessoa()
+            {
+                Console.WriteLine("jiejdiwejdowe");
+            }
+
+        }
+
+        
+
+
+
+
+
+
+
+        
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            p.name = "deded";
+            p.idade = 30;
+
+            Console.WriteLine("Resposta:", p.name, p.idade); ;
+
+            Pessoa pess = new Pessoa();
+
+            pess.name = "PP";
+            pess.idade = 3123;
+
+            pess.matricula = "212323";
+
+            Console.WriteLine(pess.name, pess.idade, pess.matricula);
+
+
+
+        }
+    }
+}
