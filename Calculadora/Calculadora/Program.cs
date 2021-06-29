bc3980194f0c6f772160601e970721afbcb01a0f
8502153 <edsonfr@sme.prefeitura.sp.gov.br>
2021-06-28 22:00:26 -0300

    test de Calcuadora

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Calculadora
+{
+
+    
+
+
+
+    
+    
+    
+    class Program
+    
+
+
+
+
+
+    {
+
+
+        public int soma(int x, int y)
+        {
+            return x + y;
+        }
+
+
+        public int sub(int x, int y)
+        {
+            return x -y;
+        }
+
+        public int mult(int x, int y)
+        {
+            return x * y;
+
+        }
+
+
+        public int div(int x, int y)
+        {
+            return x / y;
+        }
+
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 4;
+            Program p1 = new Program();
+
+            
+
+
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Soma:" + p1.soma(n1, n2));
+            Console.WriteLine("Sub:" + p1.sub(n1, n2));
+            Console.WriteLine("Mult:" + p1.mult(n1, n2));
+            Console.WriteLine("Div:" + p1.div(n1, n2));
+
+
+
+
+
+
+
+        }
+    }
+}
